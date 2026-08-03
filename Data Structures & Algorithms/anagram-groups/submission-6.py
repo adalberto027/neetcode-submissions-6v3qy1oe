fd823c7cd4f2f1e40cb3bf1ef23d2dced14cf320
adalberto027 <adalberto2772003@gmail.com>
2026-08-03 00:22:11 -0600

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        if strs == [""]:
+            return [[""]]
+        anagr_map = defaultdict(list)
+        for e in strs:
+            anagr_map[''.join(sorted(e))].append(e)
+        print(list(anagr_map.values()))
+        return list(anagr_map.values())
